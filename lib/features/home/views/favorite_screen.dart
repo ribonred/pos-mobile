a084163b668df88dbf41fc83a027e03d03356cae
wahyuarhel <akunwahyuarhel@gmail.com>
2023-10-13 16:13:37 +0700

    refactor favorite screen

--- lib/features/home/views/favorite_screen.dart
+++ lib/features/home/views/favorite_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:menu_barcode_apps/app/widgets/widgets.dart';
+import 'package:menu_barcode_apps/features/home/views/tab/bottom_tab_controller.dart';
+import 'package:menu_barcode_apps/features/home/widgets/main_page_container.dart';
+
+import '../../../app/constants/app_text_style.dart';
 
 class FavoriteScreen extends StatelessWidget {
   static const String route = '/favorite_screen';
@@ -7,11 +12,33 @@ class FavoriteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        AppText('Favorite Screen'),
-        Placeholder(),
-      ],
+    return WillPopScope(
+      onWillPop: () async {
+        int pageId = BottomTabController().indexTab.value;
+        Get.back(id: pageId);
+        return false;
+      },
+      child: MainPageContainer(
+        padding: const EdgeInsets.symmetric(horizontal: 34),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppText(
+              'Favorite Screen',
+              style: AppTextStyle.title1,
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+                child: ListView(
+              children: const [
+                Placeholder(),
+                Placeholder(),
+                Placeholder(),
+              ],
+            )),
+          ],
+        ),
+      ),
     );
   }
 

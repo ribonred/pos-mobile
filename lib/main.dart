50a0e9bb60a675ebac01bcccebba7fbdf3283ff2
rede akbar wijaya <ribonred@gmail.com>
2023-12-01 03:37:24 +0700

    request camera permissions on start

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
-
+import 'package:permission_handler/permission_handler.dart';
 import 'app.dart';
 import 'providers/pos_api.dart';
 import 'services/database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Permission.camera.onDeniedCallback(() => exit(0)).request();
   await dotenv.load();
   await initServices();
   runApp(const POSApp());
